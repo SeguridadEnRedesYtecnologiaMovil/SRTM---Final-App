540af097254e327c82e051c93eb01615ed471613
ocorona <a245329@uach.mx>
2016-07-22 16:52:12 -0600

    Fisrt try...

--- app/src/main/java/mx/uach/srtm/srtm_finalapp/MainActivity.java
+++ app/src/main/java/mx/uach/srtm/srtm_finalapp/MainActivity.java
@@ -1,13 +1,58 @@
 package mx.uach.srtm.srtm_finalapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanRecord;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BluetoothLeScanner mBluetoothLeScanner;
+    private BluetoothAdapter mBluetoothAdapter;
+    private ScanFilter mScanFilter;
+    private ScanSettings mScanSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+
     }
+
+    private void setScanFilter() {
+        ScanFilter.Builder mBuilder = new ScanFilter.Builder();
+        ByteBuffer mManufacturerData = ByteBuffer.allocate(23);
+        ByteBuffer mManufacturerDataMask = ByteBuffer.allocate(24);
+        byte[] uuid = getIdAsByte(UUID.fromString("0CF052C297CA407C84F8B62AAC4E9020");
+        mManufacturerData.put(0, (byte)0xBE);
+        mManufacturerData.put(1, (byte)0xAC);
+        for (int i=2; i<=17; i++) {
+            mManufacturerData.put(i, uuid[i-2]);
+        }
+        for (int i=0; i<=17; i++) {
+            mManufacturerDataMask.put((byte)0x01);
+        }
+        mBuilder.setManufacturerData(224, mManufacturerData.array(), mManufacturerDataMask.array());
+        mScanFilter = mBuilder.build();
+    }
+
+    private void setScanSettings() {
+        ScanSettings.Builder mBuilder = new ScanSettings.Builder();
+        mBuilder.setReportDelay(0);
+        mBuilder.setScanMode(ScanSettings.SCAN_MODE_LOW_POWER);
+        mScanSettings = mBuilder.build();
+    }
+
 }
